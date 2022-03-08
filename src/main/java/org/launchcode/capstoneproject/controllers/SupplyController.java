beefdbd685d45b62b04587fc36e17c14519952f4
AndyFitz <fitzandy4@gmail.com>
2022-03-08 17:10:46 -0500

    Creates form and form handler

--- src/main/java/org/launchcode/capstoneproject/controllers/SupplyController.java
+++ src/main/java/org/launchcode/capstoneproject/controllers/SupplyController.java
@@ -3,7 +3,9 @@ package org.launchcode.capstoneproject.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,14 +14,24 @@ import java.util.List;
 @RequestMapping("supplies")
 public class SupplyController {
 
+    private static List<String> supplies = new ArrayList<>();
+
     @GetMapping
     public String displayAllSupplies(Model model) {
-        List<String> supplies = new ArrayList<>();
-        supplies.add("Construction Paper");
-        supplies.add("Copic Markers");
-        supplies.add("Glitter");
         model.addAttribute("supplies", supplies);
         return "supplies/index";
     }
+
+    @GetMapping("create")
+    public String renderCreateSupplyForm() {
+        return "supplies/create";
+    }
+
+    @PostMapping("create")
+    public String createSupply(@RequestParam String supplyName) {
+        supplies.add(supplyName);
+        return "redirect:";
+    }
+
 
 }
